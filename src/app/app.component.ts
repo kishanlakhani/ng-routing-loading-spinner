62900ae00600a17041eff38285d3b92e7e23951b
kishanlakhani <kishan.lakhani@bacancytechnology.com>
2020-02-11 17:57:28 +0530

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
+import { Router,Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-routing-loading-spinner';
+  isLoading:boolean = false;
+
+  constructor(
+    private _router:Router
+  ){}
+
+  ngOnInit(): void {
+    this._router.events.subscribe((event:Event)=>{
+      // console.log(event);
+
+      if(event instanceof NavigationStart){
+        this.isLoading = true;
+      }
+      if(event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError){
+        setTimeout(()=>{
+          this.isLoading = false;
+        },2000)
+      }
+
+
+    })
+  }
 }
